464dded4903fd541b711e9158dbb4f11c0e1c2d2
earyzhe <earyzhe@gmail.com>
2019-07-31 13:29:14 +0200

    New folder for timer with api to world clock

--- Esp32_DevBoard_send_message_to_telegraf_via_mqtt _with_serial_communication_to_arduino_mini/sketch_jul29a_with_time_api_and_rtc/sketch_jul29a_with_time_api_and_rtc.ino
+++ Esp32_DevBoard_send_message_to_telegraf_via_mqtt _with_serial_communication_to_arduino_mini/sketch_jul29a_with_time_api_and_rtc/sketch_jul29a_with_time_api_and_rtc.ino
@@ -7,7 +7,6 @@
   #include "RTClib.h"
   RTC_DS3231 rtc;
   char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-  int currentHour = 0;
   
   // Update these with values suitable for your network.
   // Monkey Park
@@ -55,7 +54,6 @@
       setup_wifi();
       client.setServer(mqtt_server, mqtt_port);
       reconnect(); 
-      setupRtc ();
   }
 
 
@@ -90,7 +88,7 @@
         rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
         // This line sets the RTC with an explicit date &amp; time, for example to set
         // January 21, 2014 at 3am you would call:
-        //     rtc.adjust(DateTime(2019, 7, 21, 1, 30, 0));
+        // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
       }
       rtc.adjust(DateTime(__DATE__, __TIME__));
   }
@@ -172,20 +170,48 @@
     delay(10);
   }
 
-void timeLoop () {
-  DateTime now = rtc.now();;
-  if (now.hour() != currentHour) {
-    if (currentHour < 24) {
-      currentHour = now.hour();
-
-      if (now.hour() > 21 || now.hour() < 9) {
-        Serial.print("The current hour is ");
-        Serial.println(currentHour);
-        Serial2.println("TURN_OFF");
-      }
-    }
-  }
+  void timeLoop () {
+    DateTime now = rtc.now();
+     
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+     
+    Serial.print(" since midnight 1/1/1970 = ");
+    Serial.print(now.unixtime());
+    Serial.print("s = ");
+    Serial.print(now.unixtime() / 86400L);
+    Serial.println("d");
+     
+    // calculate a date which is 7 days and 30 seconds into the future
+    DateTime future (now + TimeSpan(7,12,30,6));
+     
+    Serial.print(" now + 7d + 30s: ");
+    Serial.print(future.year(), DEC);
+    Serial.print('/');
+    Serial.print(future.month(), DEC);
+    Serial.print('/');
+    Serial.print(future.day(), DEC);
+    Serial.print(' ');
+    Serial.print(future.hour(), DEC);
+    Serial.print(':');
+    Serial.print(future.minute(), DEC);
+    Serial.print(':');
+    Serial.print(future.second(), DEC);
+    Serial.println();
+     
+    Serial.println();
 }
-
-
+  
   
