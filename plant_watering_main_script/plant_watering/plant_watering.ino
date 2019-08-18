eeb2513853f73dd97e120fe5dbe9cd872f8fb465
earyzhe <earyzhe@gmail.com>
2019-08-18 12:38:06 +0200

    Improved the module script so that it can be sput to sleep or awake from an outside message via serial port

--- plant_watering_main_script/plant_watering/plant_watering.ino
+++ plant_watering_main_script/plant_watering/plant_watering.ino
@@ -6,10 +6,9 @@
 ////////////////////////////////////////////////////////////////////
 #define SYSTEMID "nano-01"
 
-
 class Module
 {
-public:
+  public:
     Module(int, int, char, int, int, int, int, int, bool, char*);
     int readPin;
     int currentPercentage;
@@ -26,16 +25,16 @@ public:
 /// Constructor for each module
 Module::Module(int a, int b, char c, int d, int e, int f, int g, int h, bool i, char* j )
 {
-    readPin = a;
-    currentPercentage = b;
-    id = c;
-    moistureSettingHigh = d;
-    servoPin = e;
-    sensorLowerValue = f;
-    sensorUpperValue = g;
-    moistureSettingLow = h;
-    isPumping = i;
-    plantType = j;
+  readPin = a;
+  currentPercentage = b;
+  id = c;
+  moistureSettingHigh = d;
+  servoPin = e;
+  sensorLowerValue = f;
+  sensorUpperValue = g;
+  moistureSettingLow = h;
+  isPumping = i;
+  plantType = j;
 }
 
 #define MODULE_COUNT 7
@@ -43,186 +42,259 @@ Module::Module(int a, int b, char c, int d, int e, int f, int g, int h, bool i, 
 const int pumpPin = 12;
 const char systemId = "plant_system";
 const float baudRate = 115200;
+bool isSleeping = false;
 
 // Time in milliseconds
-const unsigned long  loopDelayNormalSecs = 600;
-//const unsigned long  loopDelayNormalSecs = 6;
+//const unsigned long  loopDelayNormalSecs = 600;
+const unsigned long  loopDelayNormalSecs = 1;
 const int loopDelayPumpmilli = 100;
 
 Module modules[MODULE_COUNT] = {
-    Module(A0, 0, '1', 70, 2, 622, 323, 40, false, "scindapsus"),
-    Module(A1, 0, '2', 70, 3, 640, 323, 40, false, "scindapsus"),    /// Checked sensor values 2/6/2019 Plant two - Hanging plant
-    Module(A2, 0, '3', 70, 4, 622, 312, 40, false, "scindapsus"),
-    Module(A3, 0, '4', 90, 5, 664, 339, 60, false, "tray"),    // Big plant bed
-    Module(A4, 0, '5', 70, 6, 672, 308, 40, false, "bonsai"),     // Checked sensor values 8/5/2019 bonsai
-    Module(A5, 0, '6', 60, 7, 700, 372, 30, false, "cactus"),     // Checked sensor values 2/6/2019 Cactus
-    Module(A6, 0, '7', 70, 8, 597, 287, 40, false, "peace_lily"), // Checked sensor values 8/5/2019 Peace Lily
-    //        Module(A7,0,'8', 50, 9, 882, 734),
+  Module(A0, 0, '1', 70, 2, 622, 323, 40, false, "scindapsus"),
+  Module(A1, 0, '2', 70, 3, 640, 323, 40, false, "scindapsus"),    /// Checked sensor values 2/6/2019 Plant two - Hanging plant
+  Module(A2, 0, '3', 70, 4, 622, 312, 40, false, "scindapsus"),
+  Module(A3, 0, '4', 90, 5, 664, 339, 60, false, "tray"),    // Big plant bed
+  Module(A4, 0, '5', 70, 6, 672, 217, 40, false, "bonsai"),     // Checked sensor values 8/5/2019 bonsai
+  Module(A5, 0, '6', 60, 7, 700, 372, 30, false, "cactus"),     // Checked sensor values 2/6/2019 Cactus
+  Module(A6, 0, '7', 70, 8, 597, 287, 40, false, "peace_lily"), // Checked sensor values 8/5/2019 Peace Lily
+  //        Module(A7,0,'8', 50, 9, 882, 734),
 };
 
 void setup()
 {
-    Serial.begin(baudRate);
-    // Initialise pins
-    for (int i = 2; i < (MODULE_COUNT + 3); i++)
-    {
-        Serial.print("Pin ");
-        Serial.print(i);
-        pinMode(i, OUTPUT);
-        Serial.print(" is set to OUTPUT\n");
-        digitalWrite(i, HIGH);
-        delay(100);
-    }
-    pinMode(pumpPin, OUTPUT);
-    digitalWrite(pumpPin, LOW);
-    for (int i = 2; i < (MODULE_COUNT + 3); i++)
-    {
-        modules[i].isPumping = false;
-    }
-    Serial.println("Finished setup");
+  Serial.begin(baudRate);
+  // Initialise pins
+  for (int i = 2; i < (MODULE_COUNT + 3); i++)
+  {
+    Serial.print("Pin ");
+    Serial.print(i);
+    pinMode(i, OUTPUT);
+    Serial.print(" is set to OUTPUT\n");
+    digitalWrite(i, HIGH);
+    delay(100);
+  }
+  pinMode(pumpPin, OUTPUT);
+  digitalWrite(pumpPin, LOW);
+  for (int i = 2; i < (MODULE_COUNT + 3); i++)
+  {
+    modules[i].isPumping = false;
+  }
+  Serial.println("Finished setup");
 }
 
 void loop()
 {
+  if (!isSleeping) {
+    checkPlants();
+  }
+  if (Serial.available() > 0) {
+    recvWithStartEndMarkers();
+  }
+}
 
-    bool needsPump = false;
+void handleMessage(char* message) {
+  Serial.print("In handle message with the  message ");
+  Serial.println( message);
+  if (message == "SLEEP") {
+    isSleeping = true;
+  }
+  if (message == "WAKE") {
+    isSleeping = false;
+  }
+}
 
-    for (int i = 0; i < MODULE_COUNT; i++)
+void checkPlants() {
+  bool needsPump = false;
+
+  for (int i = 0; i < MODULE_COUNT; i++)
+  {
+    Module currentModule = modules[i];
+    int totalChar = strlen(systemId);
+    // Project /
+    Serial.print("<plant_system");
+    //Tags /
+    Serial.print(",thing=");
+    Serial.print(SYSTEMID);
+
+    Serial.print(",plnt_typ=");
+    Serial.print(currentModule.plantType);
+    Serial.print(",id=");
+    Serial.print(currentModule.id);
+
+    Serial.print(" ");
+
+    //Fields
+    Serial.print("serv_pin=");
+    Serial.print(currentModule.servoPin);
+
+    Serial.print(",read_pin=");
+    Serial.print(currentModule.readPin);
+
+    Serial.print(",sensor_reading=");
+    Serial.print(analogRead(currentModule.readPin));
+
+    Serial.print(",moi_set_hi=");
+    Serial.print(currentModule.moistureSettingHigh);
+
+    Serial.print(",moi_set_lo=");
+    Serial.print(currentModule.moistureSettingLow);
+
+    Serial.print(",sens_lo_val=");
+    Serial.print(currentModule.sensorLowerValue);
+
+    Serial.print(",sens_hi_val=");
+    Serial.print(currentModule.sensorUpperValue);
+    delay(1);
+
+    currentModule.currentPercentage = convertToPercent(analogRead(currentModule.readPin), currentModule);
+
+    Serial.print(",moi_lvl=");
+    Serial.print(currentModule.currentPercentage);
+
+    if (currentModule.currentPercentage < currentModule.moistureSettingLow)
     {
-        Module currentModule = modules[i];
-        int totalChar = strlen(systemId);
-        // Project /
-        Serial.print("<plant_system");
-        //Tags /
-        Serial.print(",thing=");
-        Serial.print(SYSTEMID);
-        
-        Serial.print(",plnt_typ=");
-        Serial.print(currentModule.plantType);
-        Serial.print(",id=");
-        Serial.print(currentModule.id);
-        
-        Serial.print(" ");
+      currentModule.isPumping = true;
+      needsPump = true;
+      digitalWrite(currentModule.servoPin, LOW);
 
-        //Fields
-        Serial.print("serv_pin=");
-        Serial.print(currentModule.servoPin);
+      //Opening servo
+      Serial.print(",dead_zone=0");
+    }
+    if (currentModule.currentPercentage >= currentModule.moistureSettingLow && currentModule.currentPercentage <= currentModule.moistureSettingHigh)
+    {
+      if (!currentModule.isPumping)
+      {
+        digitalWrite(currentModule.servoPin, HIGH);
+      }
 
-        Serial.print(",read_pin=");
-        Serial.print(currentModule.readPin);
+      //the deadzone
+      Serial.print(",ded_zn=1");
+    }
+    if (currentModule.currentPercentage > currentModule.moistureSettingHigh)
+    {
+      currentModule.isPumping = false;
+      digitalWrite(currentModule.servoPin, HIGH);
 
-        Serial.print(",sensor_reading=");
-        Serial.print(analogRead(currentModule.readPin));
-
-        Serial.print(",moi_set_hi=");
-        Serial.print(currentModule.moistureSettingHigh);
-
-        Serial.print(",moi_set_lo=");
-        Serial.print(currentModule.moistureSettingLow);
-
-        Serial.print(",sens_lo_val=");
-        Serial.print(currentModule.sensorLowerValue);
-
-        Serial.print(",sens_hi_val=");
-        Serial.print(currentModule.sensorUpperValue);
-        delay(1);
-
-        currentModule.currentPercentage = convertToPercent(analogRead(currentModule.readPin), currentModule);
-
-        Serial.print(",moi_lvl=");
-        Serial.print(currentModule.currentPercentage);
-        
-        if (currentModule.currentPercentage < currentModule.moistureSettingLow)
-        {
-            currentModule.isPumping = true;
-            needsPump = true;
-            digitalWrite(currentModule.servoPin, LOW);
-
-            //Opening servo
-            Serial.print(",dead_zone=0");
-        }
-        if (currentModule.currentPercentage >= currentModule.moistureSettingLow && currentModule.currentPercentage <= currentModule.moistureSettingHigh)
-        {
-            if (!currentModule.isPumping)
-            {
-                digitalWrite(currentModule.servoPin, HIGH);
-            }
-
-            //the deadzone
-            Serial.print(",ded_zn=1");
-        }
-        if (currentModule.currentPercentage > currentModule.moistureSettingHigh)
-        {
-            currentModule.isPumping = false;
-            digitalWrite(currentModule.servoPin, HIGH);
-
-            //Opening servo
-            Serial.print(",ded_zn=0");
-        }
-
-        byte servoPinState = digitalRead(currentModule.servoPin);
-        if (servoPinState == LOW)
-        {
-            Serial.print(",servo=1");
-        }
-        else
-        {
-            Serial.print(",servo=0");
-        } 
-
-        byte pumpPinState = digitalRead(pumpPin);
-        
-        if (pumpPinState == LOW)
-        {
-            Serial.println(",pmp=0>");
-        }
-        else
-        {
-            Serial.println(",pmp=1>");
-        }
-        delay(10);
+      //Opening servo
+      Serial.print(",ded_zn=0");
     }
 
-    if (needsPump)
+    byte servoPinState = digitalRead(currentModule.servoPin);
+    if (servoPinState == LOW)
     {
-        digitalWrite(pumpPin, HIGH);
-        delay(loopDelayPumpmilli);
+      Serial.print(",servo=1");
     }
     else
     {
-        digitalWrite(pumpPin, LOW);
-        int p = 0;
-        while (p < loopDelayNormalSecs) {
-        delay(1000);
-        p++;
-      }
-
+      Serial.print(",servo=0");
     }
+
+    byte pumpPinState = digitalRead(pumpPin);
+
+    if (pumpPinState == LOW)
+    {
+      Serial.println(",pmp=0>");
+    }
+    else
+    {
+      Serial.println(",pmp=1>");
+    }
+    delay(10);
+  }
+
+  if (needsPump)
+  {
+    digitalWrite(pumpPin, HIGH);
+    delay(loopDelayPumpmilli);
+  }
+  else
+  {
+    digitalWrite(pumpPin, LOW);
+    int p = 0;
+    while (p < loopDelayNormalSecs) {
+      delay(1000);
+      p++;
+    }
+
+  }
+
+}
+
+char* recvWithStartEndMarkers()
+{
+  const byte numChars = 255;
+  char receivedChars[numChars];
+  boolean newData = false;
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
+
+  while (Serial.available() > 0 && newData == false)
+  {
+    rc = Serial.read();
+
+    if (recvInProgress == true)
+    {
+      if (rc != endMarker)
+      {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars)
+        {
+          ndx = numChars - 1;
+        }
+      }
+      else
+      {
+        receivedChars[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
+      }
+    }
+
+    else if (rc == startMarker)
+    {
+      recvInProgress = true;
+    }
+  }
+  Serial.print("In recieved chars with the  message ");
+  Serial.println(receivedChars);
+  if (strcmp(receivedChars,"SLEEP")== 0) {
+    isSleeping = true;
+    Serial.println("The module is ALSEEP" );
+  }
+  if (strcmp(receivedChars,"WAKE")==0) {
+    isSleeping = false;
+    Serial.println("The module is AWAKE");
+  }  
 }
 
 int convertToPercent(int sensorValue, Module module)
 {
-    int percentValue = 0;
-    percentValue = map(sensorValue, module.sensorLowerValue, module.sensorUpperValue, 0, 100);
-    return percentValue;
+  int percentValue = 0;
+  percentValue = map(sensorValue, module.sensorLowerValue, module.sensorUpperValue, 0, 100);
+  return percentValue;
 }
 
 void printSetting(int setting)
 {
-    Serial.print("Setting: ");
-    Serial.print(setting);
-    Serial.println("%");
+  Serial.print("Setting: ");
+  Serial.print(setting);
+  Serial.println("%");
 }
 
 void printId(String id)
 {
-    Serial.print(" - Plant ");
-    Serial.println(id);
+  Serial.print(" - Plant ");
+  Serial.println(id);
 }
 
 void printValueToSerial(int currentPercentage)
 {
-    Serial.print(" - Moisture Percent: ");
-    Serial.print(currentPercentage);
-    Serial.println("%");
+  Serial.print(" - Moisture Percent: ");
+  Serial.print(currentPercentage);
+  Serial.println("%");
 }
